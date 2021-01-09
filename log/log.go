e27c7c8c50e6712a240047fc812d5eb6cfb1e3f7
junhaideng <201648748@qq.com>
2021-01-10 07:46:54 +0800

    log configuration

---
+++ log/log.go
@@ -0,0 +1,43 @@
+package log
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"os"
+	"strings"
+)
+
+var levelMap = map[string]logrus.Level{
+	"info":    logrus.InfoLevel,
+	"debug":   logrus.DebugLevel,
+	"trace":   logrus.TraceLevel,
+	"warning": logrus.WarnLevel,
+	"error":   logrus.ErrorLevel,
+}
+
+const (
+	ConsoleMode = "console"
+	FileMode    = "file"
+)
+
+func init() {
+	fmt.Println("initialize log")
+	level := viper.GetString("log.level")
+	logrus.SetLevel(levelMap[level])
+
+	mode := viper.GetString("log.mode")
+
+	if strings.EqualFold(mode, ConsoleMode) {
+
+	} else if strings.EqualFold(mode, FileMode) {
+		filename := viper.GetString("log.filename")
+		f, err := os.Create(filename)
+		if err != nil {
+			panic(err)
+		}
+		logrus.SetOutput(f)
+	} else {
+		logrus.Info("log mode specified not legal, use console mode default")
+	}
+}
