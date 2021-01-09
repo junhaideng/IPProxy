35ace0c06e2d4dc67ac1b4aa96e15fd75fcfa36f
junhaideng <201648748@qq.com>
2021-01-10 07:46:37 +0800

    configuration file

---
+++ conf/conf.go
@@ -0,0 +1,36 @@
+package conf
+
+import (
+	"github.com/spf13/viper"
+	"fmt"
+)
+
+func init() {
+	fmt.Println("initialize configuration")
+	viper.AddConfigPath("conf")
+	viper.SetConfigType("yaml")
+	setDefault()
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+}
+
+func setDefault() {
+	// 数据库配置
+	viper.SetDefault("database.mongodb.port", "27017")
+	viper.SetDefault("database.mongodb.host", "127.0.0.1")
+	viper.SetDefault("database.mongodb.db", "IP")
+
+	// 日志配置
+	viper.SetDefault("log.level", "info")
+	viper.SetDefault("log.mode", "console")
+	viper.SetDefault("log.filename", "proxy.log")
+	viper.SetDefault("log.max-size", 5)
+
+	// 定时任务配置
+	viper.SetDefault("schedule.interval", 1)
+
+	// api 接口配置
+	viper.SetDefault("api.host", "127.0.0.1")
+	viper.SetDefault("api.port", "8000")
+}
